792005f7d9bf3af86955e7acfc15bbe802612dc1
Argen7um <argen7um.music@yandex.ru>
2023-12-11 01:30:41 +0700

    add grouper data

--- parser_1.py
+++ parser_1.py
@@ -5,6 +5,8 @@ import itertools
 from typing import List, Tuple
 
 
+
+
 class Parser:
 
     def __init__(self) -> None:
@@ -25,7 +27,7 @@ class Parser:
 
     @staticmethod
     def _get_chank_data(df: pd.DataFrame, st_index: int, fin_index: int) -> List[pd.DataFrame]:
-        return df.iloc[st_index+1:fin_index].set_axis(['unid', 'active', 'passive', 'debit', 'credit'], axis=1)
+        return df.iloc[st_index+1:fin_index].set_axis(['unid', 'active', 'passive', 'debit', 'credit'], axis=1).reset_index(drop=True)
 
     def get_classes_from_excel_file(self, path: str) -> List[pd.DataFrame]:
         df = self._read_file(path)
@@ -33,3 +35,42 @@ class Parser:
         indxs = self._get_split_indexes(cl_df)
         return [self._get_chank_data(cl_df, st, fin) for st, fin in indxs]
     
+
+class Groupper(Parser):
+
+    @staticmethod
+    def _get_output_balance(df: pd.DataFrame) -> pd.DataFrame:
+        df['output_active'] = df['active'] + df['debit'] - df['credit']
+        df.loc[df['active']  == 0, 'output_active'] = 0
+
+        df['output_passive'] = df['passive'] - df['debit'] + df['credit']
+        df.loc[df['passive']  == 0, 'output_passive'] = 0
+        return df.astype('float128')
+    
+    def add_group_result(self, df: pd.DataFrame) -> pd.DataFrame:
+        conc_df = pd.DataFrame()
+        for id, gr_df in df.groupby(df.unid.apply(lambda a: int(a) // 100)):
+            res = gr_df.sum(axis=0)
+            res.unid = id
+            conc_df = pd.concat([conc_df, gr_df, pd.DataFrame(res).T]).reset_index(drop=True)
+        return conc_df
+    
+    def add_class_result(self, df: pd.DataFrame) -> pd.DataFrame:
+        result = pd.DataFrame(df[df.unid > 99].sum(axis=0)).T
+        result.unid = "ИТОГО ЗА КЛАСС"
+        return pd.concat([df, result]).reset_index(drop=True)
+    
+    def add_total_result(self, dfs: List[pd.DataFrame]) -> pd.DataFrame:
+        df = pd.concat([self.add_class_result(
+                            self.add_group_result(
+                                self._get_output_balance(df_)
+                                )
+                            ) 
+                        for df_ in dfs])
+        result = pd.DataFrame(df[df.unid == 'ИТОГО ЗА КЛАСС'].sum(axis=0)).T
+        result.unid = "ИТОГО"
+        return pd.concat([df, result]).reset_index(drop=True)
+    
+    def get_grouped_data(self, path: str) -> pd.DataFrame:
+        dfs = self.get_classes_from_excel_file(path)
+        return self.add_total_result(dfs)
