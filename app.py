ff2161550c22ade33d24f6a266eae844fd276454
Argen7um <argen7um.music@yandex.ru>
2023-12-18 09:51:24 +0700

    add migranitons

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 from parser_1 import Parser
+from db.session import init_db, get_db
+from db.models import Class
 
-from fastapi import FastAPI, UploadFile
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import Depends, FastAPI, UploadFile
 import aiofiles
 
 app = FastAPI()
 parser = Parser()
 
 
+
 @app.post("/file/upload-file")
-async def upload_file(file: UploadFile):
+async def upload_file(file: UploadFile, db: AsyncSession = Depends(get_db)):
+    print(db)
     file_path = f'files/{file.filename}'
     async with aiofiles.open(file_path, 'wb') as saved_file:
         content = await file.read()
         await saved_file.write(content)
-
-    dfs = parser.get_classes_from_excel_file(file_path)
+    try:
+        dfs = parser.get_classes_from_excel_file(file_path)
+    except :
+        pass
     print(dfs[0])
